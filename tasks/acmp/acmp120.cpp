d3d34e5da50984c9536314e4d55185d509bee70e
emli <emilbeksulaymanov@gmail.com>
2018-12-10 22:00:05 +0600

    Small fixes

--- tasks/acmp/acmp120.cpp
+++ tasks/acmp/acmp120.cpp
@@ -7,26 +7,27 @@ const int N = (int) 20 + 7;
 
 class acmp120 {
 public:
-    int sizeI{}, sizeJ{};
-
     void solve(std::istream &cin, std::ostream &cout) {
         ios::sync_with_stdio(false);
         cin.tie(0);
+        int sizeI, sizeJ;
         cin >> sizeI >> sizeJ;
-        vector<vector<int>> a(sizeI + 1ULL, vector<int>(sizeJ + 1, 0));
-        vector<vector<int>> minSum(sizeI + 1ULL, vector<int>(sizeJ + 1, INF));
+        vector<vector<int>> a(sizeI + 1, vector<int>(sizeJ + 1, 0));
+        vector<vector<int>> minSum(sizeI + 1, vector<int>(sizeJ + 1, INF));
 
         for (int i = 1; i <= sizeI; ++i) {
             for (int j = 1; j <= sizeJ; ++j) {
                 cin >> a[i][j];
             }
         }
-        minSum[1][1] = a[1][1];
 
         for (int i = 1; i <= sizeI; ++i) {
             for (int j = 1; j <= sizeJ; ++j) {
-                if (i == 1 && j == 1) continue;
-                minSum[i][j] = min(minSum[i - 1][j], minSum[i][j - 1]) + a[i][j];
+                if (i == 1 && j == 1) {
+                    minSum[i][j] = a[i][j];
+                } else {
+                    minSum[i][j] = min(minSum[i - 1][j], minSum[i][j - 1]) + a[i][j];
+                }
             }
         }
         cout << minSum[sizeI][sizeJ] << endl;
