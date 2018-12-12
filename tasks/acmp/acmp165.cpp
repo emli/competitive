c678aa57bc72b38ec3672857d20180a41e768cdb
emli <emilbeksulaymanov@gmail.com>
2018-12-12 10:31:42 +0600

    Tried to solve 165

--- tasks/acmp/acmp165.cpp
+++ tasks/acmp/acmp165.cpp
@@ -2,34 +2,16 @@
 
 using namespace std;
 
-const int N = (int) 70 + 7;
-
 class acmp165 {
 public:
-    int sizeI, sizeJ;
-
-    int solve(int x, int y, vector<vector<int>> &a) {
-        if (x == sizeI && y == sizeJ) {
-            return 1;
-        }
-        int ans = 0;
-        if (x + a[x][y] <= sizeI) {
-            ans += solve(x + a[x][y], y, a);
-        }
-        if (y + a[x][y] <= sizeJ) {
-            ans += solve(x, y + a[x][y], a);
-        }
-        return ans;
-    }
-
     void solve(std::istream &cin, std::ostream &cout) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
+        int sizeI, sizeJ;
 
         cin >> sizeI >> sizeJ;
         vector<vector<int>> countWays(size_t(sizeI + 1), vector<int>(sizeJ + 1, 0));
         vector<vector<int>> a(size_t(sizeI + 1), vector<int>(sizeJ + 1, 0));
-        vector<pair<int, int> > ways[N][N];
 
         for (int i = 1; i <= sizeI; ++i) {
             for (int j = 1; j <= sizeJ; ++j) {
@@ -41,7 +23,7 @@ public:
             for (int j = sizeJ; j >= 1; --j) {
                 if (i == sizeI && j == sizeJ) {
                     countWays[i][j] = 1;
-                    break;
+                    continue;
                 }
                 if (i + a[i][j] <= sizeI) {
                     countWays[i][j] += countWays[i + a[i][j]][j];
@@ -51,13 +33,6 @@ public:
                 }
             }
         }
-        cout << countWays[sizeI][sizeJ] << endl;
-
-        for (int i = 1; i <= sizeI; ++i) {
-            for (int j = 1; j <= sizeJ; ++j) {
-                cout << countWays[i][j] << " ";
-            }
-            cout << endl;
-        }
+        cout << countWays[1][1] << endl;
     }
 };
