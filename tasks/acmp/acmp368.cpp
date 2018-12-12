fc5deafc6ffcca0a0d3648aaed2b9e5d573f488f
emli <emilbeksulaymanov@gmail.com>
2018-12-12 13:40:24 +0600

    Improved code, variable names

--- tasks/acmp/acmp368.cpp
+++ tasks/acmp/acmp368.cpp
@@ -12,10 +12,10 @@ public:
         int n;
 
         cin >> n;
-        vector<vector<int>> bestWay(size_t(n + 1), vector<int>(n + 1, INF));
+        vector<vector<int>> minSum(size_t(n + 1), vector<int>(n + 1, INF));
         vector<vector<int>> a(size_t(n + 1), vector<int>(n + 1, 0));
         vector<vector<char>> ans(size_t(n + 1), vector<char>(size_t(n + 1), '.'));
-        vector<vector<pair<int, int>>> restoreAns(size_t(n + 1), vector<pair<int, int> >(size_t(n + 1), {0, 0}));
+        vector<vector<pair<int, int>>> from(size_t(n + 1), vector<pair<int, int> >(size_t(n + 1), {0, 0}));
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
@@ -28,14 +28,14 @@ public:
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (i == 1 && j == 1) {
-                    bestWay[i][j] = a[i][j];
-                    restoreAns[i - 1][j - 1] = {-1, -1};
-                } else if (bestWay[i - 1][j] <= bestWay[i][j - 1]) {
-                    bestWay[i][j] = bestWay[i - 1][j] + a[i][j];
-                    restoreAns[i][j] = {i - 1, j};
+                    minSum[i][j] = a[i][j];
+                    from[i][j] = {-1, -1};
+                } else if (minSum[i - 1][j] <= minSum[i][j - 1]) {
+                    minSum[i][j] = minSum[i - 1][j] + a[i][j];
+                    from[i][j] = {i - 1, j};
                 } else {
-                    bestWay[i][j] = bestWay[i][j - 1] + a[i][j];
-                    restoreAns[i][j] = {i, j - 1};
+                    minSum[i][j] = minSum[i][j - 1] + a[i][j];
+                    from[i][j] = {i, j - 1};
                 }
             }
         }
@@ -43,12 +43,14 @@ public:
         int x = n;
         int y = n;
 
-        while (restoreAns[x][y] != make_pair(-1, -1)) {
+        while (from[x][y] != make_pair(-1, -1)) {
             ans[x][y] = '#';
-            pair<int, int> temp = restoreAns[x][y];
+            pair<int, int> temp = from[x][y];
             x = temp.first;
             y = temp.second;
         }
+
+        ans[1][1] = '#';
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
