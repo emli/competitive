aa66d902a03420d8f076d51b00c41b5574b5bf84
emli <emilbeksulaymanov@gmail.com>
2018-12-13 16:14:37 +0600

    Solution for O(N)

--- tasks/acmp/acmp114.cpp
+++ tasks/acmp/acmp114.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-#define debug(x) cout << #x <<" = " << x << endl
 
 class acmp114 {
 public:
@@ -14,23 +13,11 @@ public:
         vector<int> count(size_t(n + 1), 0);
 
         count[1] = k - 1;
-
-        for (int i = 2; i <= n; ++i) {
-            count[i] += count[i - 1] * (k) - count[i - 2];
+        count[2] = (k - 1) * k;
+        for (int i = 3; i <= n; ++i) {
+            count[i] = (k - 1) * (count[i - 1] + count[i - 2]);
         }
 
-        for (int i = 1; i <= n; ++i) {
-            debug(count[i]);
-        }
         cout << count[n] << endl;
     }
 };
-
-//1 -> {1} -> 1
-//2 -> {10,11} - 2
-//3 -> {101,110,111} - 3
-//4 -> {1010,1011,1100,1101,1110,1111}
-//count[i] = 1
-//count[i] = 2
-//count[i] = 3
-//count[i] = 4
