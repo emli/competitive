65d1e163dcad1423b81a09f56cbac48b69c620ca
emli <emilbeksulaymanov@gmail.com>
2018-12-16 15:03:58 +0600

    Исправил ваш тест. Все равно WA#3

--- tasks/Agent.cpp
+++ tasks/Agent.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define all(x) (x).begin(),(x).end()
+const bool debug = false;
 
 class Agent {
 public:
@@ -21,15 +22,24 @@ public:
 
         for (int i = 1; i < n; ++i) {
             if (i == 1) {
-                minSum[i] = minSum[i - 1] + max(min(a[i - 1].second, a[i].second), min(a[i + 1].second, a[i].second));
+                minSum[i] = minSum[i - 1] + min(max(a[i - 1].second, a[i].second), max(a[i + 1].second, a[i].second));
             } else if (i == n - 1) {
                 minSum[i] = minSum[i - 1] + min(a[i - 1].second, a[i].second);
             } else {
-                minSum[i] = minSum[i - 2] + max(min(a[i - 1].second, a[i].second), min(a[i + 1].second, a[i].second));
+                minSum[i] = minSum[i - 2] + min(max(a[i - 1].second, a[i].second), max(a[i + 1].second, a[i].second));
             }
         }
 
+        if (debug) {
+            for (int i = 0; i < n; ++i) {
+                cout << i << " ";
+            }
+            cout << endl;
 
+            for (int i = 0; i < n; ++i) {
+                cout << minSum[i] << " ";
+            }
+        }
         cout << minSum[n - 1] << endl;
     }
 };
