ddab9f01f9cdf87c6aca96cfe68b6153772f33f9
emli <emilbeksulaymanov@gmail.com>
2018-12-18 16:48:34 +0600

    WA on first test

--- tasks/IgraSPeshkoi.cpp
+++ tasks/IgraSPeshkoi.cpp
@@ -3,29 +3,11 @@
 using namespace std;
 
 const int N = 101;
-int maxAns[N][N][2];
+int maxAns[N][N];
 
 class IgraSPeshkoi {
 public:
     vector<int> a;
-
-    int go(int l, int r, int first) {
-        int &res = maxAns[l][r][first];
-        if (res != -1) {
-            return res;
-        }
-        if (l == r) {
-            return res = a[l];
-        }
-        if (l < r) {
-            if (first) {
-                return res = max(go(l + 1, r, 1 - first), go(l, r - 1, 1 - first));
-            } else {
-                return res = min(go(l + 1, r, 1 - first), go(l, r - 1, 1 - first));
-            }
-        }
-    }
-
     void solve(std::istream &cin, std::ostream &cout) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
@@ -40,20 +22,20 @@ public:
             for (int left = 0; left + len - 1 < n; ++left) {
                 int right = left + len - 1;
                 for (int first = 1; first >= 0; --first) {
-                    int &res = maxAns[left][right][first];
+                    int &res = maxAns[left][right];
                     if (left == right) {
                         res = a[left];
                     }
                     if (left < right) {
                         if (first) {
-                            res = max(maxAns[left + 1][right][1 - first], maxAns[left][right - 1][1 - first]);
+                            res = max(maxAns[left + 1][right], maxAns[left][right - 1]);
                         } else {
-                            res = min(maxAns[left + 1][right][1 - first], maxAns[left][right - 1][1 - first]);
+                            res = min(maxAns[left + 1][right], maxAns[left][right - 1]);
                         }
                     }
                 }
             }
         }
-        cout << maxAns[0][n - 1][1] << endl;
+        cout << maxAns[0][n - 1] << endl;
     }
 };
