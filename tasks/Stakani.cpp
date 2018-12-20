51b6c20b08c6c01ad597c3e2860ba621419a0682
emli <emilbeksulaymanov@gmail.com>
2018-12-20 21:07:48 +0600

    954 Just for TL

---
+++ tasks/Stakani.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Stakani {
+public:
+    int k;
+
+    string lastFourLetter(string s) {
+        if (s.length() <= 4) {
+            return s;
+        }
+        return s.substr(s.size() - 4);
+    }
+
+    int go(int c, const string &s, const string &all) {
+        if (c == k) {
+            return 1;
+        }
+        int ans = 0;
+        if (c == 0) {
+            ans += go(c + 10, s + "A", all + "A");
+            ans += go(c + 10, s + "B", all + "B");
+        } else if (c < k) {
+            for (char x : {'A', 'B'}) {
+                string temp = s + x;
+                temp = lastFourLetter(temp);
+                if (temp == "AAAA" || temp == "BBBB") {
+                    continue;
+                }
+                if (s.back() == x) {
+                    ans += go(c + 1, temp, all + x);
+                } else {
+                    ans += go(c + 10, temp, all + x);
+                }
+            }
+        }
+        return ans;
+    }
+
+    void solve(std::istream &cin, std::ostream &cout) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cin >> k;
+        cout << go(0, "", "") << endl;
+    }
+};
+
