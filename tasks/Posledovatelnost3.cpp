65fce27059117c3d86975ea042446afedf4d7a15
emli <emilbeksulaymanov@gmail.com>
2018-12-20 12:19:46 +0600

    Improved 789

--- tasks/Posledovatelnost3.cpp
+++ tasks/Posledovatelnost3.cpp
@@ -10,24 +10,26 @@ public:
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
 
-        priority_queue<ll, vector<ll>, greater<ll> > priority_queue1;
-        priority_queue1.push(1);
+        priority_queue<ll, vector<ll>, greater<ll> > pq;
+        pq.push(1);
 
 
         vector<ll> ans;
 
-        while (ans.size() < 10000) {
-            ll minNum = priority_queue1.top();
+        ll x, minNum = 0, len = 0;
+        cin >> x;
+
+        while (len < x) {
+            minNum = pq.top();
             ans.push_back(minNum);
             for (ll x : {2, 3, 5}) {
-                priority_queue1.push(minNum * x);
+                pq.push(minNum * x);
             }
-            while (!priority_queue1.empty() && minNum == priority_queue1.top())
-                priority_queue1.pop();
+            while (minNum == pq.top())
+                pq.pop();
+            len++;
         }
 
-        ll x;
-        cin >> x;
-        cout << ans[x - 1] << endl;
+        cout << minNum << endl;
     }
 };
