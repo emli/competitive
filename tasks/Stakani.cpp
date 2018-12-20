4c4d362e22712143a5492685833a4f78d2d9b417
emli <emilbeksulaymanov@gmail.com>
2018-12-21 00:04:40 +0600

    954 Accepted

--- tasks/Stakani.cpp
+++ tasks/Stakani.cpp
@@ -2,21 +2,13 @@
 
 using namespace std;
 
-int mod = static_cast<int>(1e6);
+int mod = (1e6);
 const int N = 1e5 + 10;
-int dp[N][256][5];
+int dp[N][3][5];
 class Stakani {
 public:
     int k;
-
-    string lastFourLetter(string s) {
-        if (s.length() <= 4) {
-            return s;
-        }
-        return s.substr(s.size() - 4);
-    }
-    int go(int c, char s,int count) {
-        cout << count << endl;
+    int go(int c, int s,int count) {
         int &res = dp[c][s][count];
         if (res != -1){
             return res;
@@ -26,7 +18,7 @@ public:
         }
         int ans = 0;
         if (c < k) {
-            for (char x : {'A', 'B'}) {
+            for (int x : {0, 1}) {
                 int temp = 0;
                 if (s == x){
                     temp = count + 1;
@@ -37,7 +29,7 @@ public:
                 if (s == x) {
                     ans += go(c + 1, s,temp) % mod;
                 } else {
-                    ans += go(c + 10, s,temp) % mod;
+                    ans += go(c + 10, x,1) % mod;
                 }
             }
         }
@@ -48,8 +40,8 @@ public:
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         cin >> k;
-        char x = 0;
-        memset(dp,-1, sizeof(dp));
+        int x = 2;
+        memset(dp,-1, sizeof dp);
         cout << go(0, x,0) << endl;
     }
 };
