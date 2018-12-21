25cc0b70f7f3f22fe992f60e00e74e5d1c9ca2b7
emli <emilbeksulaymanov@gmail.com>
2018-12-21 17:41:56 +0600

    335 WA

--- tasks/TriprostieChisla.cpp
+++ tasks/TriprostieChisla.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const int N = (int) 1e4 + 7;
 int mod = 1e9 + 9;
 
-int dp[N][1000];
+int dp[2][1000];
 
 class TriprostieChisla {
 public:
@@ -19,27 +19,29 @@ public:
 
         int answer = 0;
         int change = 1;
-        for (int k = 100; k < 1000; ++k) {
-            if (isPrime(k)) {
-                for (int len = n; len >= 3; --len) {
-                    for (int last3digits = 100; last3digits <= 999; ++last3digits) {
-                        int &res = dp[change][last3digits];
-                        if (len == n) {
-                            res = 1;
-                        }
-                        if (len < n) {
-                            int ans = 0;
-                            for (int i = 1; i <= 9; ++i) {
-                                int t = (last3digits * 10 + i) % 1000;
-                                if (isPrime(t) && t >= 100) {
-                                    ans += dp[1 - change][t] % mod;
-                                }
-                            }
-                            res = ans;
+        vector<int> primes = allPrimes(1000);
+        for (int len = n; len >= 3; --len) {
+            for (int last3digits = 100; last3digits <= 999; ++last3digits) {
+                int &res = dp[change][last3digits];
+                if (len == n) {
+                    res = 1;
+                }
+                if (len < n) {
+                    int ans = 0;
+                    for (int i = 1; i <= 9; ++i) {
+                        int t = (last3digits * 10 + i) % 1000;
+                        if (isPrime(t) && t >= 100) {
+                            ans += dp[1 - change][t] % mod;
                         }
                     }
-                    change = 1 - change;
+                    res = ans;
                 }
+            }
+            change = 1 - change;
+        }
+
+        for (int k : primes) {
+            if (k >= 100) {
                 answer += dp[1 - change][k] % mod;
             }
         }
