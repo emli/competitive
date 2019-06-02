d85cffb9e9304235c1f3ed4e5bcddc8f21678ac4
emli <emilbeksulaymanov@gmail.com>
2019-06-02 14:59:25 +0600

    Binary Search

---
+++ tasks/RUKiddingMrFeynman.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+class RUKiddingMrFeynman {
+public:
+	void solve(std::istream& cin, std::ostream& cout) {
+		ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        double x;
+        double eps = 0.0001;
+        while (cin  >> x){
+            if (x == 0){
+                break;
+            }
+            double l = 1;
+            double r = x;
+
+            while (r - l > eps){
+                double m = (l + r) / 2.0;
+
+                if (pow(m,3.0) - x < eps){
+                    l = m;
+                }else {
+                    r = m;
+                }
+            }
+
+            cout << fixed << setprecision(4) << l << endl;
+        }
+	}
+};
