90b04a402d48d32378853cc02b833fb67b018e78
emli <emilbeksulaymanov@gmail.com>
2022-01-03 14:50:41 +0600

    Solved https://codeforces.com/contest/120/problem/C

---
+++ tasks/CVinniPukhIMed.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+const int dx[] = {1,-1,0,0};
+const int dy[] = {0,0,1,-1};
+class CVinniPukhIMed {
+public:
+    static void solve(std::istream& cin, std::ostream& cout) {
+        int n,k;
+        cin >> n >> k;
+        vector<int> a(n);
+
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        ranges :: sort(a);
+
+        vector<int> count(n);
+
+        int ans = 0;
+        for (int i = n - 1; i >= 0; --i) {
+
+            for (int j = 0; j < 3; ++j) {
+                if (a[i] >= k){
+                    a[i] -= k;
+                }
+            }
+            ans += a[i];
+        }
+
+        cout << ans << endl;
+        
+    }
+};
